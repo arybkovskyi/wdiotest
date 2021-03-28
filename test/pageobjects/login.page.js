e7582821bc8b1693af7d934dda5f82199861479e
Alex Rybkovskyi <alex.rybkovskyi@gmail.com>
2021-03-27 21:33:22 -0500

    added 3 test cases

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -3,24 +3,42 @@ import Page from './page';
 
 class LoginPage extends Page {
 
-    get inputUsername () { return $('#normal_login_email') }
-    get inputPassword () { return $('#normal_login_password') }
-    get btnSubmit () { return $('.login-form-button') }
+    get inputUsername () { return $('#normal_login_email'); };
+    get inputPassword () { return $('#normal_login_password'); };
+    get btnSubmit () { return $('.login-form-button'); };
+    get loginValidationError(){return $('//div[contains(@class, "ant-form-item-with-help")][.//input[@id="normal_login_email"]]//div[@role="alert"]'); };
+    get wrongDataMessage () { return $('.ant-notification-notice-message')}
+
 
     setLogin (email){
         this.inputUsername.setValue(email);
-    }
+    };
     setPassword (password){
         this.inputPassword.setValue(password);
-    }
+    };
     clickSubmitButton(){
         this.btnSubmit.click();
-    }
-
-
+    };
     open () {
         return super.open('/');
+    };
+
+    emptyLoginInput(){
+        this.clearInput(this.inputUsername);
+    };
+
+    submitButtonIsDisabled(){
+        expect(this.btnSubmit).toBeDisabled();
+    };
+
+    invalidData(){
+        expect(this.wrongDataMessage).toBeDisplayed();
     }
+
+     loginRequiredError(){
+        expect(this.loginValidationError).toBeDisplayed();
+        expect(this.loginValidationError.getText()).toEqual('Required')
+    };
 }
 
 export default new LoginPage();
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -2,15 +2,42 @@ import LoginPage from  '../pageobjects/login.page';
 import ProfilePage from '../pageobjects/profile.page';
 
 describe('Auth', () => {
-    it('user logs in with valid data', () => {
+    beforeEach (()=>{
         LoginPage.open();
-        LoginPage.setLogin('farmere4@lbzannualj.com');
+    });
+
+    afterEach(()=>{
+        browser.execute('window.localStorage.clear()');
+    });
+
+
+    it('user logs in with valid data', () => {
+        // LoginPage.setLogin('farmere4@lbzannualj.com');
+        LoginPage.inputUsername.setValue('farmere4@lbzannualj.com');
         LoginPage.setPassword('P@ssw0rd!');
         LoginPage.clickSubmitButton();
-        ProfilePage.isOpen()
-
-
+        ProfilePage.isOpen();
     });
+   it('test 002 Login button disabled by default', () => {
+       LoginPage.submitButtonIsDisabled();
+   });
+   it('test 003 Invalid Email results in Error Message', ()=> {
+        LoginPage.setLogin('asdasdasda@test.com');
+        LoginPage.setPassword('12345');
+        LoginPage.clickSubmitButton();
+        LoginPage.invalidData();
+   })
+
+
+
+    it('Test 005 Login Input Is Required ', function () {
+        LoginPage.setLogin('example');
+        LoginPage.emptyLoginInput();
+        LoginPage.loginRequiredError();
+    });
+
 });
+
+
 
 
